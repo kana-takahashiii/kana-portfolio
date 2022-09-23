a845d3594c4467e1558e1e210061283ec364dfa0
kana-takahashiii <kana92105@gmial.com>
2022-09-23 19:02:52 +0900

    制作物リンクを修正

--- dist/js/sample.js
+++
@@ -1,1 +0,0 @@
-const animes=gsap.utils.toArray(".js__demo");animes.forEach(e=>{gsap.from(e,{opacity:0,duration:3,x:"30%",ease:"Power4.easeOut",scrollTrigger:{trigger:item,start:"top center",onEnter:()=>item.classList.add("is-active"),onLeaveBack:()=>item.classList.remove("is-active")}})});
